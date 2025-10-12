9b4ef8efc22127255cd733dbe76870766a43ee41
ati-shay <93589539+ati-shay@users.noreply.github.com>
2025-10-12 10:30:57 +0530

    update redirect URI for Netlify deployment

--- fitness-app-frontend/src/authConfig.js
+++ fitness-app-frontend/src/authConfig.js
@@ -2,7 +2,7 @@ export const authConfig = {
     clientId: 'oauth2-pkce-client',
     authorizationEndpoint: 'http://localhost:8181/realms/fitness-oauth2/protocol/openid-connect/auth',
     tokenEndpoint: 'http://localhost:8181/realms/fitness-oauth2/protocol/openid-connect/token',
-    redirectUri: 'http://localhost:5173',
+    redirectUri: 'https://fitnessapp-frontend.netlify.app',
     scope: 'openid profile email offline_access',
     onRefreshTokenExpire: (event) => event.logIn(),
 }
